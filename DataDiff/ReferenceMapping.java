b8a0c69880d3c6b057358ea2ced8c73cf4a385d9
Evan Yang <evanjyang4@gmail.com>
2021-07-28 22:22:27 -0700

    update class properties

--- DataDiff/ReferenceMapping.java
+++ DataDiff/ReferenceMapping.java
@@ -1,20 +1,20 @@
 public class ReferenceMapping {
-    String activeID;
+    boolean active;
     String consumerType;
     String ct_code;
-    int value;
+    String value;
 
     // getters
-    public String getActiveID() { return activeID; }
+    public boolean getActive() { return active; }
     public String getConsumerType() { return consumerType; }
     public String get_ct_code() { return ct_code; }
-    public int getValue() { return value; }
+    public String getValue() { return value; }
 
     // setters
-    public void setActiveID(String activeID) { this.activeID = activeID; }
+    public void setActiveID(boolean activeID) { this.active = activeID; }
     public void setConsumerType(String consumerType) { this.consumerType = consumerType; } 
     public void set_ct_code(String ct_code) { this.ct_code = ct_code; }
-    public void setValue(int value) { this.value = value; }
+    public void setValue(String value) { this.value = value; }
 
     @Override
     public boolean equals(Object obj) {
@@ -23,7 +23,7 @@ public class ReferenceMapping {
         if (!(obj instanceof ReferenceMapping))
             return false;
         ReferenceMapping rm = (ReferenceMapping) obj;
-        return rm.getActiveID() == this.getActiveID()
+        return rm.getActive() == this.getActive()
                 && rm.getConsumerType() == this.getConsumerType()
                 && rm.get_ct_code() == this.get_ct_code()
                 && rm.getValue() == this.getValue();
@@ -32,10 +32,10 @@ public class ReferenceMapping {
     @Override
     public int hashCode() {
         int result=31;
-        result = 89 * result + (activeID !=null ? activeID.hashCode() : 0);
+        result = 89 * result + (active ? 1 : 0);
         result = 89 * result + (consumerType !=null ? consumerType.hashCode() : 0);
         result = 89 * result + (ct_code !=null ? ct_code.hashCode() : 0);
-        result += value;
+        result += value.hashCode();
         return result;
     }
 
